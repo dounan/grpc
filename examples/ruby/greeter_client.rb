42ae37487370c74ac7d174274a415f5dd3f73542
Dounan Shi <dounan@flexport.com>
2020-03-17 15:44:59 -0700

    Minimal changes to show resource exhaustion error

--- examples/ruby/greeter_client.rb
+++ examples/ruby/greeter_client.rb
@@ -28,8 +28,14 @@ require 'helloworld_services_pb'
 def main
   stub = Helloworld::Greeter::Stub.new('localhost:50051', :this_channel_is_insecure)
   user = ARGV.size > 0 ?  ARGV[0] : 'world'
-  message = stub.say_hello(Helloworld::HelloRequest.new(name: user)).message
-  p "Greeting: #{message}"
+
+  for i in 0..10000
+    begin
+      message = stub.say_hello(Helloworld::HelloRequest.new(name: user)).message
+    rescue StandardError => e
+      puts e
+    end
+  end
 end
 
 main
